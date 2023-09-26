8b16660db07ad899bd3d81eea6ade64c197d7353
chandanaavirneni <146082946+chandanaavirneni@users.noreply.github.com>
2023-09-26 15:54:38 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,12 @@
+var score=5;
+function decrease(){
+    score=score-5;
+    document.getElementById("score").textContent=score;
+    
+}
+
+function increase(){
+    score=score+5;
+    document.getElementById("score").textContent=score;
+    
+}
